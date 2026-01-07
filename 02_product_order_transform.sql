ccdf9be0ff8229b78551b582d4970d6ac8394a1a
Mr. Sayan Ghosh <162343504+Sayan520@users.noreply.github.com>
2026-01-07 19:03:05 +0530

    Update 02_product_order_transform.sql

--- 02_product_order_transform.sql
+++ 02_product_order_transform.sql
@@ -1,27 +1,33 @@
 INSERT INTO dim_products (product_id, product_name, category)
 SELECT DISTINCT
-    p.product_id,
-    p.product_name,
-    p.category
-FROM src_products p;
+    product_id,
+    product_name,
+    category
+FROM src_products;
 
 INSERT INTO stg_orders (
-    order_id, customer_id, product_id, order_date, quantity, total_amount
+    order_id,
+    customer_id,
+    product_id,
+    order_date,
+    quantity,
+    total_amount
 )
 SELECT
-    o.order_id,
-    o.customer_id,
-    o.product_id,
-    o.order_date,
-    o.quantity,
-    o.quantity * p.price AS total_amount
-FROM src_orders o
-JOIN src_products p
-    ON o.product_id = p.product_id;
+    src_orders.order_id,
+    src_orders.customer_id,
+    src_orders.product_id,
+    src_orders.order_date,
+    src_orders.quantity,
+    src_orders.quantity * src_products.price
+FROM src_orders
+JOIN src_products
+    ON src_orders.product_id = src_products.product_id;
 
-MERGE INTO stg_orders t
-USING src_orders s
-ON t.order_id = s.order_id
+MERGE INTO stg_orders
+USING src_orders
+ON stg_orders.order_id = src_orders.order_id
 WHEN MATCHED THEN
 UPDATE SET
-    t.quantity = s.quantity;
+    quantity = src_orders.quantity;
+
