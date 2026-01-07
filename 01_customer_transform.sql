2907f73cfe7aebfb070a6d1caeab1f927abd398a
Mr. Sayan Ghosh <162343504+Sayan520@users.noreply.github.com>
2026-01-07 19:02:16 +0530

    Update 01_customer_transform.sql

--- 01_customer_transform.sql
+++ 01_customer_transform.sql
@@ -1,28 +1,32 @@
 INSERT INTO dim_customers (customer_id, full_name, email, country)
-SELECT 
-    c.customer_id,
-    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
-    LOWER(c.email) AS email,
-    c.country
-FROM src_customers c
-WHERE c.is_active = 1;
+SELECT
+    customer_id,
+    CONCAT(first_name, ' ', last_name),
+    LOWER(email),
+    country
+FROM src_customers
+WHERE is_active = 1;
 
 INSERT INTO dim_customers (customer_id, full_name, email, country)
-SELECT 
-    s.customer_id,
-    CONCAT(s.first_name, ' ', s.last_name),
-    LOWER(s.email),
-    s.country
-FROM src_customers s
-LEFT JOIN dim_customers d
-    ON s.customer_id = d.customer_id
-WHERE d.customer_id IS NULL;
+SELECT
+    src_customers.customer_id,
+    CONCAT(src_customers.first_name, ' ', src_customers.last_name),
+    LOWER(src_customers.email),
+    src_customers.country
+FROM src_customers
+LEFT JOIN dim_customers
+    ON src_customers.customer_id = dim_customers.customer_id
+WHERE dim_customers.customer_id IS NULL;
 
-MERGE INTO dim_customers d
-USING src_customers s
-ON d.customer_id = s.customer_id
-WHEN MATCHED AND 
-     (d.email <> LOWER(s.email) OR d.country <> s.country)
+MERGE INTO dim_customers
+USING src_customers
+ON dim_customers.customer_id = src_customers.customer_id
+WHEN MATCHED
+AND (
+    dim_customers.email <> LOWER(src_customers.email)
+    OR dim_customers.country <> src_customers.country
+)
 THEN UPDATE SET
-    d.email = LOWER(s.email),
-    d.country = s.country;
+    email = LOWER(src_customers.email),
+    country = src_customers.country;
+
