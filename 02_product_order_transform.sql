78539c791ad60ac1f1d61a76189fa2899223966c
Mr. Sayan Ghosh <162343504+Sayan520@users.noreply.github.com>
2026-01-08 19:20:17 +0530

    Update 02_product_order_transform.sql

--- 02_product_order_transform.sql
+++ 02_product_order_transform.sql
@@ -1,4 +1,9 @@
-
+INSERT INTO dim_products (product_id, product_name, category)
+SELECT DISTINCT
+    product_id,
+    product_name,
+    category
+FROM src_products;
 
 INSERT INTO stg_orders (
     order_id,
